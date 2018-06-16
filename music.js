0497f1b9337d2f2dc50c951cc19f20e2fa61e14d
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-16 10:04:49 +0700

    Update music.js

--- music.js
+++ music.js
@@ -163,7 +163,12 @@ async function handleVideo(video, msg, voiceChannel, playlist = false) {
 		serverQueue.songs.push(song);
 		console.log(serverQueue.songs);
 		if (playlist) return undefined;
-		else return msg.channel.send(`✅ **${song.title}** has been added to the queue!`);
+		else {
+	let songadd = new RichEmbed()
+	.setDescription(`✅ **${song.title}** has ben added to the queue!`)
+	.setColor("RANDOM")
+	msg.channel.send(songadd)
+}
 	}
 	return undefined;
 }
