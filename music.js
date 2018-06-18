cf67db77b8f4a9da265b022f063f29c45bce4a8f
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-18 09:32:51 +0700

    Update music.js

--- music.js
+++ music.js
@@ -1,5 +1,5 @@
 const { Client, Util } = require('discord.js');
-const { PREFIX, GOOGLE_API_KEY } = require('./settings');
+const { PREFIX, GOOGLE_API_KEY } = require('./settings.json');
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 
